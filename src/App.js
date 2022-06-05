af5275b80e5cd9ef4b5700030c7be4dfd5b1f904
opkort <okportenko@yandex.ru>
2022-06-05 05:02:28 +0300

    create/delete posts, sort by name/description

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript 1', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'},
+    ])
+    const [selectedSort, setSelectedSort] = useState('')
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+        setPosts(
+            [...posts].sort(
+                (a,b) => a[sort].localeCompare(b[sort])
+            )
+        )
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PostForm create={createPost}/>
+        <hr style={{margin: '15px 0'}}/>
+        <div>
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue="Sort"
+                options={[
+                    {value: 'title', name: 'By name'},
+                    {value: 'body', name: 'By description'}
+                ]}
+            />
+        </div>
+        {posts.length
+            ?
+            <PostList remove={removePost} posts={posts} title="Post list"/>
+            :
+            <h1 style={{textAlign: 'center'}}>
+                Posts not found
+            </h1>
+        }
     </div>
   );
 }
